378b2171f268562cd4d3a535417d9f393bf1161a
Florian Fregien <f.fregien@hotmail.de>
2024-04-09 17:27:58 +0200

    feat(go/lib): add json encoding to config struct

--- go/lib/mandelbrot.go
+++ go/lib/mandelbrot.go
@@ -11,117 +11,120 @@ import (
 )
 
 type MandelbrotConfig struct {
-    ImageWidth, ImageHeight, MaxIterations, ThreadCount int
-    RealCenter, ImagCenter, RealWidth float64
+	ImageWidth    int     `json:"imageWidth"`
+	ImageHeight   int     `json:"imageHeight"`
+	MaxIterations int     `json:"maxIterations"`
+	ThreadCount   int     `json:"threadCount"`
+	RealCenter    float64 `json:"realCenter"`
+	ImagCenter    float64 `json:"imagCenter"`
+	RealWidth     float64 `json:"realWidth"`
 }
 
 const escapeRadius float64 = 2.0
 
 func inferRealBounds(c *MandelbrotConfig) (realStart float64, realEnd float64) {
-    realStart = c.RealCenter - 0.5 * c.RealWidth
-    realEnd = c.RealCenter + 0.5 * c.RealWidth
+	realStart = c.RealCenter - 0.5*c.RealWidth
+	realEnd = c.RealCenter + 0.5*c.RealWidth
 
-    return
+	return
 }
 
 func inferImagBounds(c *MandelbrotConfig) (imagStart float64, imagEnd float64) {
-    imagHeight := (c.RealWidth * float64(c.ImageHeight)) / float64(c.ImageWidth)
-    imagStart = c.ImagCenter - 0.5 * imagHeight
-    imagEnd = c.ImagCenter + 0.5 * imagHeight
+	imagHeight := (c.RealWidth * float64(c.ImageHeight)) / float64(c.ImageWidth)
+	imagStart = c.ImagCenter - 0.5*imagHeight
+	imagEnd = c.ImagCenter + 0.5*imagHeight
 
-    return
+	return
 }
 
 func computePixel(x0, y0 float64, maxIterations int) color.RGBA {
-    var x, y float64 = 0.0, 0.0
-    i := 0
+	var x, y float64 = 0.0, 0.0
+	i := 0
 
-    for {
-        if i == maxIterations {
-            return color.RGBA{0, 0, 0, 255}
-        }
+	for {
+		if i == maxIterations {
+			return color.RGBA{0, 0, 0, 255}
+		}
 
-        if x * x + y * y > escapeRadius * escapeRadius {
-            break
-        }
+		if x*x+y*y > escapeRadius*escapeRadius {
+			break
+		}
 
-        x, y = x * x - y * y + x0, 2.0 * x * y + y0
-        i++
-    }
+		x, y = x*x-y*y+x0, 2.0*x*y+y0
+		i++
+	}
 
-    h := 360.0 - (360.0 * float64(i) / float64(maxIterations))
-    r, g, b := hsvToRgb(h, 0.8, 0.8)
+	h := 360.0 - (360.0 * float64(i) / float64(maxIterations))
+	r, g, b := hsvToRgb(h, 0.8, 0.8)
 
-    return color.RGBA{r, g, b, 255}
+	return color.RGBA{r, g, b, 255}
 }
 
 func generateMandelbrotChunk(img *image.RGBA, c *MandelbrotConfig, rows int, offset int) {
-    realStart, realEnd := inferRealBounds(c)
-    imagStart, imagEnd := inferImagBounds(c)
+	realStart, realEnd := inferRealBounds(c)
+	imagStart, imagEnd := inferImagBounds(c)
 
-    for y := offset; y < offset + rows; y++ {
-        y0 := float64(y) * ((imagEnd - imagStart) / float64(img.Rect.Dy())) + imagStart
+	for y := offset; y < offset+rows; y++ {
+		y0 := float64(y)*((imagEnd-imagStart)/float64(img.Rect.Dy())) + imagStart
 
-        for x := 0; x < img.Rect.Dx(); x++ {
-            x0 := float64(x) * ((realEnd - realStart) / float64(img.Rect.Dx())) + realStart
-            pix := computePixel(x0, y0, c.MaxIterations)
+		for x := 0; x < img.Rect.Dx(); x++ {
+			x0 := float64(x)*((realEnd-realStart)/float64(img.Rect.Dx())) + realStart
+			pix := computePixel(x0, y0, c.MaxIterations)
 
-            img.Set(x, img.Rect.Dy() - y, pix)
-        }
-    }
+			img.Set(x, img.Rect.Dy()-y, pix)
+		}
+	}
 }
 
 func generateMandelbrotSequentially(w io.Writer, c MandelbrotConfig) error {
-    img := image.NewRGBA(image.Rect(0, 0, c.ImageWidth, c.ImageHeight))
-    realStart, realEnd := inferRealBounds(&c)
-    imagStart, imagEnd := inferImagBounds(&c)
+	img := image.NewRGBA(image.Rect(0, 0, c.ImageWidth, c.ImageHeight))
+	realStart, realEnd := inferRealBounds(&c)
+	imagStart, imagEnd := inferImagBounds(&c)
 
-    for y := 0; y < img.Rect.Dy(); y++ {
-        y0 := float64(y) * ((imagEnd - imagStart) / float64(img.Rect.Dy())) + imagStart
+	for y := 0; y < img.Rect.Dy(); y++ {
+		y0 := float64(y)*((imagEnd-imagStart)/float64(img.Rect.Dy())) + imagStart
 
-        for x := 0; x < img.Rect.Dx(); x++ {
-            x0 := float64(x) * ((realEnd - realStart) / float64(img.Rect.Dx())) + realStart
-            pix := computePixel(x0, y0, c.MaxIterations)
+		for x := 0; x < img.Rect.Dx(); x++ {
+			x0 := float64(x)*((realEnd-realStart)/float64(img.Rect.Dx())) + realStart
+			pix := computePixel(x0, y0, c.MaxIterations)
 
-            img.Set(x, img.Rect.Dy() - y, pix)
-        }
-    }
+			img.Set(x, img.Rect.Dy()-y, pix)
+		}
+	}
 
-    return png.Encode(w, img)
+	return png.Encode(w, img)
 }
 
 func generateMandelbrotConcurrently(w io.Writer, c MandelbrotConfig) error {
-    var wg sync.WaitGroup
-    img := image.NewRGBA(image.Rect(0, 0, c.ImageWidth, c.ImageHeight))
-    threadCount := int(math.Min(float64(c.ThreadCount), float64(img.Rect.Dy())))
+	var wg sync.WaitGroup
+	img := image.NewRGBA(image.Rect(0, 0, c.ImageWidth, c.ImageHeight))
+	threadCount := int(math.Min(float64(c.ThreadCount), float64(img.Rect.Dy())))
 
-    if threadCount < c.ThreadCount {
-        fmt.Printf("warning: number of thread was clamped to %d\n", threadCount)
-    }
+	if threadCount < c.ThreadCount {
+		fmt.Printf("warning: number of thread was clamped to %d\n", threadCount)
+	}
 
-    chunkSize := img.Rect.Dy() / threadCount
+	chunkSize := img.Rect.Dy() / threadCount
 
-    for i := 0; i < threadCount; i++ {
-        wg.Add(1)
-        i := i
+	for i := 0; i < threadCount; i++ {
+		wg.Add(1)
+		i := i
 
-        go func() {
-            defer wg.Done()
-            generateMandelbrotChunk(img, &c, chunkSize, i * chunkSize)
-        }()
-    }
+		go func() {
+			defer wg.Done()
+			generateMandelbrotChunk(img, &c, chunkSize, i*chunkSize)
+		}()
+	}
 
-    wg.Wait()
+	wg.Wait()
 
-    return png.Encode(w, img)
+	return png.Encode(w, img)
 }
 
 func GenerateMandelbrot(w io.Writer, c MandelbrotConfig) error {
-    if c.ThreadCount == 1 {
-        return generateMandelbrotSequentially(w, c)
-    } else {
-        return generateMandelbrotConcurrently(w, c)
-    }
+	if c.ThreadCount == 1 {
+		return generateMandelbrotSequentially(w, c)
+	} else {
+		return generateMandelbrotConcurrently(w, c)
+	}
 }
-
-
